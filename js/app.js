15671103b5f605c23a9eeda26f259c6797851f27
Roy A Vanegas <roy@thecodeeducators.com>
2015-12-12 22:48:17 -0500

    Implement 4-way toggling

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ window.onload = function () {
         firstChildOfBody = body.firstElementChild,
         gridLayer = document.createElement('div'),
         styleSheet = document.styleSheets[0],
+        gridChoice = 1;
 
     gridLayer.setAttribute('id', 'column-baseline-grid');
 
@@ -21,10 +22,39 @@ window.onload = function () {
         body.textContent = 'The body element does not have a child element.';
     }
 
+    gridLayer.setAttribute('class', 'all-grids');
+
     document.onkeydown = function (evnt) {
         if (27 === evnt.keyCode) {
+            switch (gridChoice) {
+            case 0:
+                gridLayer.classList.add('all-grids');
+
+                break;
+            case 1:
+                gridLayer.classList.remove('all-grids');
+                gridLayer.classList.add('modular-grid');
+
+                break;
+            case 2:
+                gridLayer.classList.remove('modular-grid');
+                gridLayer.classList.add('column-grid');
+
+                break;
+            case 3:
+                gridLayer.classList.remove('column-grid');
+                gridLayer.classList.add('baseline-grid');
+
+                break;
+            case 4:
+                gridLayer.classList.remove('baseline-grid');
+
+                break;
             }
 
+            if (gridChoice++ === 4) {
+                gridChoice = 0;
+            }
         }
     };
 };
