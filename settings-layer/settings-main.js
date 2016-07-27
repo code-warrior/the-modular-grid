017ce9e8d3981bede67ff89210ab7839b15585b6
Saman Tehrani <samanrtehrani@gmail.com>
2016-07-27 19:01:28 -0400

    simple on/off switch added in settings layer in form of a browser action

---
+++ settings-layer/settings-main.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+   let powerSwitch = window.document.getElementById('power-switch');
+
+   chrome.runtime.sendMessage('settingsOpened',function(response){
+      console.log(response.gridIsEnabled);
+      powerSwitch.checked = response.gridIsEnabled;
+   });
+
+   powerSwitch.addEventListener('change',function(){
+      if(powerSwitch.checked){
+         chrome.runtime.sendMessage('enableGrid');
+      }
+      else{
+         chrome.runtime.sendMessage('disableGrid');
+      }
+   },false);
+   
+};
