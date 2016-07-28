cd702561d8e23f23689ea2b9260b93245860027d
Roy A Vanegas <roy@thecodeeducators.com>
2016-07-28 19:22:43 -0400

    Format comments

--- backend-layer/main.js
+++ backend-layer/main.js
@@ -1,22 +1,24 @@
 /* global chrome*/
 
 let lastTabId;
-//structure for holding and reserving project settings
+
+// Structure for holding and reserving project settings
 currentSettings = {
    gridIsEnabled : false
 };
 
-//acquiring last tab's ID
+// Acquiring last tab’s ID
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
    lastTabId = tabs[0].id;
 });
 
-//maitaining track of the current tab ID
+// Maitaining track of the current tab ID
 chrome.tabs.onSelectionChanged.addListener(function(tabId) {
    lastTabId = tabId;
 });
 
-//message event listener -- To be replaced with a persistent communication port instead of a single time message passing
+// Message event listener -- To be replaced with a persistent communication port
+// instead of a single time message passing
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
    switch(message) {
       case 'settingsOpened':
