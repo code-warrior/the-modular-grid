198a1a8ba3755105c729b8a11dcee0b32ac202b0
Roy A Vanegas <roy@thecodeeducators.com>
2016-12-25 14:50:50 -0500

    Format main.js in settings layer

--- settings-layer/main.js
+++ settings-layer/main.js
@@ -1,17 +1,17 @@
 window.onload = function() {
-   let powerSwitch = window.document.getElementById('power-switch');
+    let powerSwitch = document.getElementById('popup--switch__input');
 
-   chrome.runtime.sendMessage('settingsOpened',function(response){
-      console.log(response.gridIsEnabled);
-      powerSwitch.checked = response.gridIsEnabled;
-   });
+    chrome.runtime.sendMessage('settingsOpened', function(response) {
+        console.log(response.gridIsEnabled);
+        powerSwitch.checked = response.gridIsEnabled;
+    });
 
-   powerSwitch.addEventListener('change',function(){
-      if(powerSwitch.checked){
-         chrome.runtime.sendMessage('enableGrid');
-      }
-      else{
-         chrome.runtime.sendMessage('disableGrid');
-      }
-   },false);
+    powerSwitch.addEventListener('change', function() {
+        if (powerSwitch.checked) {
+            chrome.runtime.sendMessage('enableGrid');
+        }
+        else {
+            chrome.runtime.sendMessage('disableGrid');
+        }
+    }, false);
 };
