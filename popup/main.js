0a18b04f163302887844c09199f063113f7e6601
Roy A Vanegas <roy@thecodeeducators.com>
2016-12-28 17:11:13 -0500

    Rename `settings-layer` folder to `popup`

--- popup/main.js
+++ popup/main.js
@@ -1,17 +1,16 @@
 window.onload = function() {
     let powerSwitch = document.getElementById('popup--switch__input');
 
-    chrome.runtime.sendMessage('settingsOpened', function(response) {
-        console.log(response.gridIsEnabled);
+    chrome.runtime.sendMessage('browserActionIconClicked', function(response) {
         powerSwitch.checked = response.gridIsEnabled;
     });
 
     powerSwitch.addEventListener('change', function() {
         if (powerSwitch.checked) {
-            chrome.runtime.sendMessage('enableGrid');
+            chrome.runtime.sendMessage('gridEnabled');
         }
         else {
-            chrome.runtime.sendMessage('disableGrid');
+            chrome.runtime.sendMessage('gridDisabled');
         }
     }, false);
 };
