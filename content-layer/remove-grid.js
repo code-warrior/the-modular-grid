1bb89165e7c35752b8e1915cd01199b3bbc43984
Roy A Vanegas <roy@thecodeeducators.com>
2016-12-29 17:01:25 -0500

    Add the script that removes the grid when the button is pressed

---
+++ content-layer/remove-grid.js
@@ -0,0 +1,7 @@
+/*jslint browser:true */
+
+//
+// TODO: Using let instead of var is causing a problem.
+//
+var columnBaselineGrid = document.getElementById('column-baseline-grid');
+columnBaselineGrid.parentNode.removeChild(columnBaselineGrid);
