18bc7dbbcefaa9b1fd69d2a421e885ffe2571a1e
Roy A Vanegas <roy@thecodeeducators.com>
2016-12-31 18:35:48 -0500

    Add newlines

--- content/remove-grid.js
+++ content/remove-grid.js
@@ -1,9 +1,11 @@
 /*jslint browser:true */
+
 let error_styles =
     'background-color: green; color: white; padding: 2px; border-radius: 4px;';
 
 if (undefined !== gridBGLayer) {
     gridBGLayer.parentNode.removeChild(gridBGLayer);
+
     console.log('%c gridBGLayer removed ', error_styles);
 }
 
