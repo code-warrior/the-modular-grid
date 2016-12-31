826b72f1286e8b47923e3887b41c6586e400931a
Roy A Vanegas <roy@thecodeeducators.com>
2016-12-31 18:04:13 -0500

    Report when the gridBGLayer reference is removed

--- content/remove-grid.js
+++ content/remove-grid.js
@@ -1,7 +1,11 @@
 /*jslint browser:true */
+let error_styles =
+    'background-color: green; color: white; padding: 2px; border-radius: 4px;';
 
 if (undefined !== gridBGLayer) {
     gridBGLayer.parentNode.removeChild(gridBGLayer);
+    console.log('%c gridBGLayer removed ', error_styles);
+}
 
     console.log('%c gridBGLayer removed ',
         'background-color: green; color: white; padding: 4px; border-radius: 4px;');
