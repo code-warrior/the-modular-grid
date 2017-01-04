d6d5538c13b2570eb35caf71a8c856c3fea57e73
Roy A Vanegas <roy@thecodeeducators.com>
2017-01-03 21:22:02 -0500

    Add the beginnings of the option for column width

---
+++ options/options.js
@@ -0,0 +1,36 @@
+/*jslint browser: true */
+/*global window, chrome */
+
+/**
+ * Saves options to chrome.storage
+ */
+function save_options() {
+    let columnWidth = document.getElementById('column--width-input').value;
+
+    chrome.storage.sync.set({
+        columnWidth: columnWidth
+    }, function() {
+        let status = document.getElementById('status');
+
+        status.textContent = 'Options saved.';
+
+        setTimeout(function() {
+            status.textContent = '';
+        }, 1500);
+    });
+}
+
+/**
+ * Restores select box and checkbox state using the preferences stored in
+ * chrome.storage.
+ */
+function restore_options() {
+    chrome.storage.sync.get({
+        columnWidth: '60'
+    }, function(items) {
+        document.getElementById('column--width-input').value = items.columnWidth;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save-options').addEventListener('click', save_options);
