3ac8399d8dc8c75a3db3b9d2dc4a7fee10b137bb
Roy A Vanegas <roy@thecodeeducators.com>
2017-01-09 23:36:17 -0500

    Implement the gutter width option

--- options/options.js
+++ options/options.js
@@ -6,11 +6,13 @@
  */
 function save_options() {
     let gridColumn = document.getElementById('column--width-input').value;
+    let gridGutter = document.getElementById('gutter--width-input').value;
     let baselineColor = document.getElementById('baseline--color-input').value;
     let baselineDistance = document.getElementById('baseline--vertical-distance-input').value;
 
     chrome.storage.sync.set({
         gridColumn: gridColumn,
+        gridGutter: gridGutter,
         baselineColor: baselineColor,
         baselineDistance: baselineDistance
 
@@ -31,12 +33,14 @@ function save_options() {
 function restore_options() {
     chrome.storage.sync.get({
         gridColumn: '60',
+        gridGutter: '20',
         baselineColor: '#29abe2',
         baselineDistance: '24'
     }, function(items) {
         document.getElementById('column--width-input').value = items.gridColumn;
         document.getElementById('baseline--color-input').value = items.baselineColor;
         document.getElementById('baseline--vertical-distance-input').value = items.baselineDistance;
+        document.getElementById('gutter--width-input').value = items.gridGutter;
     });
 }
 
