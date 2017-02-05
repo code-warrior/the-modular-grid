bcf284903ac5abf6b01fc6cc2dc3127b53b6028d
Roy A Vanegas <roy@thecodeeducators.com>
2017-02-05 12:50:30 -0500

    Add task to copy unprocessed, raw files to the extension folder

--- gulpfile.js
+++ gulpfile.js
@@ -25,3 +25,23 @@ gulp.task('default', function () {
                 'available tasks are:\n\n' + stdout);
     });
 });
+
+gulp.task('copyRawFilesToExtensionFolder', function () {
+    'use strict';
+
+    return gulp.src([
+        'src/img/**/*',
+        'src/_locales/**/*',
+        'src/fonts/**/*',
+        'src/manifest.json',
+        '!src/img/extension-installed.png',
+        '!src/img/extension-loaded-into-github-desktop.png',
+        '!src/img/folder-and-browser.png',
+        '!src/img/saving-repo.png',
+        '!src/img/screenshot--baseline-grid.png',
+        '!src/img/screenshot--column-grid.png',
+        '!src/img/screenshot--modular-grid.png',
+        '!src/img/screenshot--settings-1.png',
+        '!src/img/screenshot--settings-2.png'
+    ], {base: './src/'}).pipe(gulp.dest('extension'));
+});
