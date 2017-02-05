3106b82bb55a7c5387e3f595060b633322e626e1
Roy A Vanegas <roy@thecodeeducators.com>
2017-02-05 12:52:27 -0500

    Move default task to the tail of the file

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,6 @@ var gulp = require('gulp'),
     config = require('./config.json'),
     colors = config.colors;
 
-gulp.task('default', function () {
-    'use strict';
-
-    var exec = require('child_process').exec;
-
-    exec('gulp --tasks', function (error, stdout, stderr) {
-        if (null !== error) {
-            process.stdout.write('An error was likely generated when invoking ' +
-                    'the `exec` program in the default task.');
-        }
-
-        if ('' !== stderr) {
-            process.stdout.write('Content has been written to the stderr stream ' +
-                    'when invoking the `exec` program in the default task.');
-        }
-
-        process.stdout.write('\n\tThis default task does ' + colors.red +
-                'nothing' + colors.default + ' but generate this message. The ' +
-                'available tasks are:\n\n' + stdout);
-    });
-});
-
 gulp.task('copyRawFilesToExtensionFolder', function () {
     'use strict';
 
@@ -44,4 +22,26 @@ gulp.task('copyRawFilesToExtensionFolder', function () {
         '!src/img/screenshot--settings-1.png',
         '!src/img/screenshot--settings-2.png'
     ], {base: './src/'}).pipe(gulp.dest('extension'));
+});
+
+gulp.task('default', function () {
+    'use strict';
+
+    var exec = require('child_process').exec;
+
+    exec('gulp --tasks', function (error, stdout, stderr) {
+        if (null !== error) {
+            process.stdout.write('An error was likely generated when invoking ' +
+                'the `exec` program in the default task.');
+        }
+
+        if ('' !== stderr) {
+            process.stdout.write('Content has been written to the stderr stream ' +
+                'when invoking the `exec` program in the default task.');
+        }
+
+        process.stdout.write('\n\tThis default task does ' + colors.red +
+            'nothing' + colors.default + ' but generate this message. The ' +
+            'available tasks are:\n\n' + stdout);
+    });
 });
