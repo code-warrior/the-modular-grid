e1ea3e0258803a7f0fa1730342e873dda44f27e5
Roy A Vanegas <roy@thecodeeducators.com>
2017-05-16 22:20:00 -0400

    Update extension

--- extension/options.js
+++ extension/options.js
@@ -120,6 +120,26 @@ function populateOptionsFormWithStorageOptions() {
     );
 }
 
+/**
+ * Fill the grid-related Sass variables section with values retrieved from storage.
+ */
+function populateSassGridVariablesWithStorageOptions() {
+    'use strict';
+
+    chrome.storage.sync.get(
+        null,
+        function (settings) {
+
+            let variableValues = document.querySelectorAll('pre span');
+
+            variableValues[0].textContent = settings.gridColumnWidth;
+            variableValues[1].textContent = settings.gridGutterWidth;
+            variableValues[2].textContent = settings.gridColumnCount;
+            variableValues[3].textContent = settings.gridBaselineDistance;
+        }
+    );
+}
+
 //
 // Columns
 // — Width
@@ -129,6 +149,7 @@ document.getElementById('column--width-input').addEventListener('focus', functio
 
     document.onkeydown = function (evnt) {
         let columnWidthInputBoxValue = document.getElementById('column--width-input').value,
+            columnWidthGridVarValue = document.querySelector('pre > kbd:first-of-type > span'),
             columnWidthInputBox__ErrorMessage = document.getElementById('width-input--error-message');
 
         columnWidthInputBoxValue = parseFloat(columnWidthInputBoxValue);
@@ -141,6 +162,7 @@ document.getElementById('column--width-input').addEventListener('focus', functio
                 columnWidthInputBox__ErrorMessage.style.display = 'none';
                 columnWidthInputBoxValue = columnWidthInputBoxValue + 1;
                 document.getElementById('column--width-input').value = columnWidthInputBoxValue;
+                columnWidthGridVarValue.textContent = columnWidthInputBoxValue;
                 saveOptions();
             }
 
@@ -157,6 +179,7 @@ document.getElementById('column--width-input').addEventListener('focus', functio
                     columnWidthInputBox__ErrorMessage.style.display = 'none';
                     columnWidthInputBoxValue = columnWidthInputBoxValue - 1;
                     document.getElementById('column--width-input').value = columnWidthInputBoxValue;
+                    columnWidthGridVarValue.textContent = columnWidthInputBoxValue;
                     saveOptions();
                 }
             }
@@ -170,14 +193,17 @@ document.getElementById('column--width-input').addEventListener('input', functio
     'use strict';
 
     let columnWidthInputBoxValue = document.getElementById('column--width-input').value,
+        columnWidthGridVarValue = document.querySelector('pre > kbd:first-of-type > span'),
         columnWidthInputBox__ErrorMessage = document.getElementById('width-input--error-message');
 
     columnWidthInputBoxValue = parseFloat(columnWidthInputBoxValue);
 
     if (isNaN(columnWidthInputBoxValue)) {
         columnWidthInputBox__ErrorMessage.style.display = 'inline';
+        columnWidthGridVarValue.innerHTML = '<span class="error-message displayed-in-a-tighter-space">Invalid column width entry</span>';
     } else {
         columnWidthInputBox__ErrorMessage.style.display = 'none';
+        columnWidthGridVarValue.textContent = columnWidthInputBoxValue;
         saveOptions();
     }
 }, false);
@@ -191,6 +217,7 @@ document.getElementById('column--count-input').addEventListener('focus', functio
 
     document.onkeydown = function (evnt) {
         let columnCountInputBoxValue = document.getElementById('column--count-input').value,
+            columnCountGridVarValue = document.querySelector('pre > kbd:nth-of-type(4) > span'),
             columnCountInputBox__ErrorMessage = document.getElementById('column-count-input--error-message');
 
         columnCountInputBoxValue = parseInt(columnCountInputBoxValue, 10);
@@ -199,10 +226,12 @@ document.getElementById('column--count-input').addEventListener('focus', functio
         case UP_ARROW_KEY:
             if (isNaN(columnCountInputBoxValue)) {
                 columnCountInputBox__ErrorMessage.style.display = 'inline';
+                columnCountGridVarValue.innerHTML = '<span class="error-message displayed-in-a-tighter-space">Invalid column count entry</span>';
             } else {
                 columnCountInputBox__ErrorMessage.style.display = 'none';
                 columnCountInputBoxValue = columnCountInputBoxValue + 1;
                 document.getElementById('column--count-input').value = columnCountInputBoxValue;
+                columnCountGridVarValue.textContent = columnCountInputBoxValue;
                 saveOptions();
             }
 
@@ -215,10 +244,12 @@ document.getElementById('column--count-input').addEventListener('focus', functio
                 if (columnCountInputBoxValue < (COLUMN_COUNT_MIN + 1)) {
                     columnCountInputBox__ErrorMessage.style.display = 'inline';
                     columnCountInputBoxValue = columnCountInputBoxValue - 1;
+                    columnCountGridVarValue.innerHTML = '<span class="error-message displayed-in-a-tighter-space">Invalid column count entry</span>';
                 } else {
                     columnCountInputBox__ErrorMessage.style.display = 'none';
                     columnCountInputBoxValue = columnCountInputBoxValue - 1;
                     document.getElementById('column--count-input').value = columnCountInputBoxValue;
+                    columnCountGridVarValue.textContent = columnCountInputBoxValue;
                     saveOptions();
                 }
             }
@@ -233,14 +264,17 @@ document.getElementById('column--count-input').addEventListener('input', functio
     'use strict';
 
     let columnCountInputBoxValue = document.getElementById('column--count-input').value,
+        columnCountGridVarValue = document.querySelector('pre > kbd:nth-of-type(4) > span'),
         columnCountInputBox__ErrorMessage = document.getElementById('column-count-input--error-message');
 
     columnCountInputBoxValue = parseInt(columnCountInputBoxValue, 10);
 
     if (isNaN(columnCountInputBoxValue)) {
         columnCountInputBox__ErrorMessage.style.display = 'inline';
+        columnCountGridVarValue.innerHTML = '<span class="error-message displayed-in-a-tighter-space">Invalid column count entry</span>';
     } else {
         columnCountInputBox__ErrorMessage.style.display = 'none';
+        columnCountGridVarValue.textContent = columnCountInputBoxValue;
         saveOptions();
     }
 }, false);
@@ -275,6 +309,8 @@ document.getElementById('gutter--width-input').addEventListener('focus', functio
     document.onkeydown = function (evnt) {
         let gutterWidthInputBoxValue =
                     document.getElementById('gutter--width-input').value,
+            gutterWidthGridVarValue =
+                    document.querySelector('pre > kbd:nth-of-type(2) > span'),
             gutterWidthInputBox__ErrorMessage =
                     document.getElementById('gutter-width-input--error-message');
 
@@ -284,11 +320,13 @@ document.getElementById('gutter--width-input').addEventListener('focus', functio
         case UP_ARROW_KEY:
             if (isNaN(gutterWidthInputBoxValue)) {
                 gutterWidthInputBox__ErrorMessage.style.display = 'inline';
+                gutterWidthGridVarValue.innerHTML = '<span class="error-message displayed-in-a-tighter-space">Invalid gutter width entry</span>';
             } else {
                 gutterWidthInputBox__ErrorMessage.style.display = 'none';
                 gutterWidthInputBoxValue = gutterWidthInputBoxValue + 1;
                 document.getElementById('gutter--width-input').value =
                         gutterWidthInputBoxValue;
+                gutterWidthGridVarValue.textContent = gutterWidthInputBoxValue;
                 saveOptions();
             }
 
@@ -301,11 +339,13 @@ document.getElementById('gutter--width-input').addEventListener('focus', functio
                 if (gutterWidthInputBoxValue < (GUTTER_WIDTH_MIN + 1)) {
                     gutterWidthInputBox__ErrorMessage.style.display = 'inline';
                     gutterWidthInputBoxValue = gutterWidthInputBoxValue - 1;
+                    gutterWidthGridVarValue.innerHTML = '<span class="error-message displayed-in-a-tighter-space">Invalid gutter width entry</span>';
                 } else {
                     gutterWidthInputBox__ErrorMessage.style.display = 'none';
                     gutterWidthInputBoxValue = gutterWidthInputBoxValue - 1;
                     document.getElementById('gutter--width-input').value =
                             gutterWidthInputBoxValue;
+                    gutterWidthGridVarValue.textContent = gutterWidthInputBoxValue;
                     saveOptions();
                 }
             }
@@ -321,13 +361,19 @@ document.getElementById('gutter--width-input').addEventListener('input', functio
 
     let gutterWidthInputBoxValue =
                 document.getElementById('gutter--width-input').value,
+        gutterWidthGridVarValue =
+            document.querySelector('pre > kbd:nth-of-type(2) > span'),
         gutterWidthInputBox__ErrorMessage =
                 document.getElementById('gutter-width-input--error-message');
 
+    gutterWidthInputBoxValue = parseFloat(gutterWidthInputBoxValue);
+
     if (isNaN(gutterWidthInputBoxValue)) {
         gutterWidthInputBox__ErrorMessage.style.display = 'inline';
+        gutterWidthGridVarValue.innerHTML = '<span class="error-message displayed-in-a-tighter-space">Invalid gutter width entry</span>';
     } else {
         gutterWidthInputBox__ErrorMessage.style.display = 'none';
+        gutterWidthGridVarValue.textContent = gutterWidthInputBoxValue;
         saveOptions();
     }
 }, false);
@@ -361,6 +407,8 @@ document.getElementById('baseline--vertical-distance-input').addEventListener('f
 
     document.onkeydown = function (evnt) {
         let baselineDistanceInputBoxValue = document.getElementById('baseline--vertical-distance-input').value,
+            baselineDistanceGridVarValue =
+                document.querySelector('pre > kbd:nth-of-type(5) > span'),
             baselineDistanceInputBox__ErrorMessage = document.getElementById('baseline-vertical-distance-input--error-message');
 
         baselineDistanceInputBoxValue = parseFloat(baselineDistanceInputBoxValue);
@@ -375,9 +423,11 @@ document.getElementById('baseline--vertical-distance-input').addEventListener('f
                 if (baselineDistanceInputBoxValue > BASELINE_DISTANCE_MAX) {
                     baselineDistanceInputBoxValue = baselineDistanceInputBoxValue - 1;
                     baselineDistanceInputBox__ErrorMessage.style.display = 'inline';
+                    baselineDistanceGridVarValue.innerHTML = '<span class="error-message displayed-in-a-tighter-space">Invalid leading distance entry</span>';
                 } else {
                     baselineDistanceInputBox__ErrorMessage.style.display = 'none';
                     document.getElementById('baseline--vertical-distance-input').value = baselineDistanceInputBoxValue;
+                    baselineDistanceGridVarValue.textContent = baselineDistanceInputBoxValue;
                     saveOptions();
                 }
             }
@@ -393,9 +443,11 @@ document.getElementById('baseline--vertical-distance-input').addEventListener('f
                 if (baselineDistanceInputBoxValue < BASELINE_DISTANCE_MIN) {
                     baselineDistanceInputBoxValue = baselineDistanceInputBoxValue + 1;
                     baselineDistanceInputBox__ErrorMessage.style.display = 'inline';
+                    baselineDistanceGridVarValue.innerHTML = '<span class="error-message displayed-in-a-tighter-space">Invalid leading distance entry</span>';
                 } else {
                     baselineDistanceInputBox__ErrorMessage.style.display = 'none';
                     document.getElementById('baseline--vertical-distance-input').value = baselineDistanceInputBoxValue;
+                    baselineDistanceGridVarValue.textContent = baselineDistanceInputBoxValue;
                     saveOptions();
                 }
             }
@@ -410,18 +462,23 @@ document.getElementById('baseline--vertical-distance-input').addEventListener('i
     'use strict';
 
     let patternForBaselineVerticalDistanceInputBox = /^([1][2-9]|[2-9][0-9]|[1][0-2][0-8])$/,
+        baselineVerticalDistanceGridVarValue =
+            document.querySelector('pre > kbd:nth-of-type(5) > span'),
         baselineVerticalDistanceInputBoxValue = document.getElementById('baseline--vertical-distance-input').value,
         baselineVerticalDistanceInputBox__ErrorMessage = document.getElementById('baseline-vertical-distance-input--error-message');
 
     if (null !== baselineVerticalDistanceInputBoxValue.match(patternForBaselineVerticalDistanceInputBox)) {
         baselineVerticalDistanceInputBox__ErrorMessage.style.display = 'none';
+        baselineVerticalDistanceGridVarValue.textContent = baselineVerticalDistanceInputBoxValue;
         saveOptions();
     } else {
         baselineVerticalDistanceInputBox__ErrorMessage.style.display = 'inline';
+        baselineVerticalDistanceGridVarValue.innerHTML = '<span class="error-message displayed-in-a-tighter-space">Invalid leading distance entry</span>';
     }
 }, false);
 
 document.addEventListener('DOMContentLoaded', populateOptionsFormWithStorageOptions, false);
+document.addEventListener('DOMContentLoaded', populateSassGridVariablesWithStorageOptions, false);
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
