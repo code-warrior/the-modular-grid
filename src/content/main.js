fc38692d7c7c7d3526ce7d5712233f2a9d148ea3
Roy A Vanegas <roy@thecodeeducators.com>
2017-11-12 08:43:35 -0500

    Error check the opacity parameter

--- src/content/main.js
+++ src/content/main.js
@@ -106,6 +106,25 @@ function convertHexToRGBA(hex, opacity) {
     let calculateNibble = 0;
     let rgba = 'rgba(';
     let index;
+    let _opacity = opacity;
+
+    if (!Number.isNaN(Number(_opacity))) {
+        _opacity = parseFloat(_opacity, 10);
+    }
+
+    alert(typeof _opacity);
+
+    if (typeof _opacity !== 'number') {
+        console.error('The opacity variable must be a number');
+
+        return;
+    } else {
+        if ((_opacity < 0.0) || (_opacity > 1.0)) {
+            console.error('The opacity variable fall within the range of 0.0 – 1.0');
+
+            return;
+        }
+    }
 
     const HEX = 16;
     const END_OF_HEX = 6;
@@ -189,7 +208,7 @@ function convertHexToRGBA(hex, opacity) {
         // We’ve arrived at the end of the conversion, so append the opacity and the
         // closing of the string.
         //
-        rgba = rgba + opacity + ')';
+        rgba = rgba + _opacity + ')';
     } else {
         return -1;
     }
